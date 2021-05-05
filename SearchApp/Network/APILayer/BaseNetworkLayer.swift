20f6e5b24b225c6dd88e2414bb17f9fc72790667
Cem Kazım <cemkazim@hotmail.com>
2021-05-05 11:20:23 +0300

    base network layer added

---
+++ SearchApp/Network/APILayer/BaseNetworkLayer.swift
@@ -0,0 +1,31 @@
+//
+//  BaseNetworkLayer.swift
+//  SearchApp
+//
+//  Created by Cem Kazım on 5.05.2021.
+//
+
+import Alamofire
+import RxSwift
+
+class BaseNetworkLayer {
+    
+    static let shared = BaseNetworkLayer()
+    
+    private init() {}
+    
+    func request<T: Decodable>(requestUrl: String, requestMethod: HTTPMethod, requestParameters: Parameters? = nil) -> Observable<T> {
+        return Observable.create { observer -> Disposable in
+            AF.request(requestUrl, method: requestMethod, parameters: requestParameters, encoding: URLEncoding.default).response { (response) in
+                guard let remoteData = response.data else { return }
+                do {
+                    let localData = try JSONDecoder().decode(T.self, from: remoteData)
+                    observer.onNext(localData)
+                } catch let error {
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
